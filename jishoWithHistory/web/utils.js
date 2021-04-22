6822d0363e7a7e6d4888c73ef647fc50b59ecbce
JaPNaA <leonehuang100@gmail.com>
2021-04-22 15:19:21 -0400

    Add IPInput component

--- jishoWithHistory/web/utils.js
+++ jishoWithHistory/web/utils.js
@@ -71,6 +71,88 @@ export class Modal extends Component {
     }
 }
 
+export class IPInput extends Component {
+    constructor() {
+        super("ipInput");
+
+        this.ipPartInputs = this._createIPPartInputs();
+        this.portInput = new Elm("input").class("port");
+        /** @type {HTMLInputElement} */
+        (this.portInput.elm).value = "65535";
+
+        this.append(
+            this.ipPartInputs[0],
+            ".",
+            this.ipPartInputs[1],
+            ".",
+            this.ipPartInputs[2],
+            ".",
+            this.ipPartInputs[3],
+            ":",
+            this.portInput
+        );
+    }
+
+    getValue() {
+        const ipParts = [];
+        for (const input of this.ipPartInputs) {
+            ipParts.push(
+                /** @type {HTMLInputElement} */
+                (input.elm).value
+            );
+        }
+
+        return ipParts.join(".") + ":" +
+            /** @type {HTMLInputElement} */
+            (this.portInput.elm).value;
+    }
+
+    /** @param {string} value */
+    setValue(value) {
+        const splits = value.split(/[\.:]/g);
+
+        for (let i = 0; i < 4; i++) {
+            /** @type {HTMLInputElement} */
+            (this.ipPartInputs[i].elm).value = splits[i];
+        }
+
+        /** @type {HTMLInputElement} */
+        (this.portInput.elm).value = splits[4];
+    }
+
+    /** @param {number|undefined} inputIndex */
+    focus(inputIndex) {
+        if (inputIndex === undefined) {
+            /** @type {HTMLInputElement} */
+            (this.ipPartInputs[0].elm).select();
+        } else {
+            if (inputIndex >= 4) {
+                /** @type {HTMLInputElement} */
+                (this.portInput.elm).select();
+            } else {
+                /** @type {HTMLInputElement} */
+                (this.ipPartInputs[inputIndex].elm).select();
+            }
+        }
+    }
+
+    _createIPPartInputs() {
+        const arr = [];
+        for (let i = 0; i < 4; i++) {
+            arr.push(
+                new Elm("input")
+                    .class("part")
+                    .attribute("type", "number")
+                    .withSelf(e =>
+                        /** @type {HTMLInputElement}*/
+                        (e.elm).value = "255"
+                    )
+            );
+        }
+        return arr;
+    }
+}
+
 /**
  * @template T
  * @param {T[]} arr1
